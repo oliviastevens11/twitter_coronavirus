9ce27dcde0295c9c76e9f5f201bc974ceee9fa83
oliviastevens11 <oliviamstevens@yahoo.com>
2023-04-20 20:31:42 -0700

    test pt.3

--- src/visualize.py
+++ src/visualize.py
@@ -29,8 +29,9 @@ for k,v in items:
     print(k,':',v)
 
 #print graphs of values
-x_axis = [item[0] for item in sorted(items[:9], key=lambda x: x[1], reverse = True)]
-y_axis = [item[1] for item in sorted(items[:9], key=lambda x: x[1], reverse = True)]
+items_sorted = sorted(items, key=lambda x: x[1], reverse=True)
+x_axis = [item[0] for item in items_sorted[:9]]
+y_axis = [item[1] for item in items_sorted[:9]]
 plt.bar(x_axis, y_axis)
 plt.title('Number of Times ' + args.key + ' Used')
 plt.xlabel(args.input_path)
